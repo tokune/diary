2fcf74ad6f984526250249961c76b6467f8523a6
tokune <tokune@gmail.com>
2014-07-16 23:44:59 +0800

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var orm = require('orm');
+var config = require('./config');
+var models = require('./lib/models');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var login_middleware = require('./lib/middleware').login_middleware;
+var app = express();
+var mysql = config.mysql;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+app.use(session({
+    secret: 'tokunejane'
+}))
+app.use(login_middleware);
+app.use(
+    orm.express(
+        "mysql://"+mysql.user+":"+mysql.password+"@"+mysql.host+"/"+mysql.db, 
+        models
+    )   
+);
+
+require('./lib/index').route(app);
+require('./lib/diary').route(app);
+
+app.listen(config.port);
---
+++ config.sample.js
@@ -0,0 +1,6 @@
+module.exports = {             
+    mysql:{host:'127.0.0.1', user:'tokune', password:'tokune', db:'diary'},
+    uid:'1644274944',                 
+    weibo:{key:6954759,secret:'6b61ee39f4f3e4658e1907f4c6410', oauth_link: 'http://127.0.0.1:5566/oauth'},
+    port:5566,                 
+}
